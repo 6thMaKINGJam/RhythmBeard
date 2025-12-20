3770999236221fb0f44110f294ebe5ffe1250410
yoodii <yoodii205@gmail.com>
2025-12-20 16:24:39 +0900

    몬스터 충돌, 하트 순서 보완

--- Assets/Script/Monster.cs
+++ Assets/Script/Monster.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RedMonster : MonoBehaviour
+public class Monster : MonoBehaviour
 {
    [Header("몬스터 설정")]
     public int hp = 1; 
+    public int attackDamage = 1; // 몬스터의 공격력 (기본 1)
     public float knockbackForce = 7f;
     public GameObject deathEffect; // 죽을 때 나올 효과 (선택사항)
     
@@ -26,16 +27,12 @@ public class RedMonster : MonoBehaviour
             ApplyKnockback(attackerPosition);
         }
     }
-    void ApplyKnockback(Vector2 attackerPosition)
+    public void ApplyKnockback(Vector2 playerPosition)
     {
-        Vector2 knockbackDirection = (Vector2)transform.position - attackerPosition;
-        
-        knockbackDirection = knockbackDirection.normalized;
-
-        rb.velocity = Vector2.zero; 
-        rb.AddForce(knockbackDirection * knockbackForce, ForceMode2D.Impulse);
-        
-        //Debug.Log("몬스터가 밀려납니다! 남은 HP: " + hp);
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        Vector2 knockbackDir = ((Vector2)transform.position - playerPosition).normalized;
+        rb.velocity = Vector2.zero;
+        rb.AddForce(knockbackDir * 5f, ForceMode2D.Impulse);
     }
     void Die()
     { 
--- Assets/Script/PlayerHeart.cs
+++ Assets/Script/PlayerHeart.cs
@@ -9,21 +9,25 @@ public class PlayerHeart : MonoBehaviour
     public int currentHealth;
 
     public GameObject[] hearts;
-    // Start is called before the first frame update
+    public GameObject burstEffectPrefab; // [꼭 확인] 인스펙터에서 하늘색 프리팹을 연결해야 합니다.
+
     void Start()
     {
         currentHealth = maxHealth;
+        for (int i = 0; i < hearts.Length; i++)
+    {
+        if (hearts[i] != null)
+            hearts[i].SetActive(true);
+    }
         UpdateUI();
     }
 
-   public void TakeDamage(int amount)
+    public void TakeDamage(int amount)
     {
         currentHealth -= amount;
-
         if (currentHealth < 0) currentHealth = 0;
-
-        //Debug.Log("플레이어 HP: " + currentHealth);
         
+        // 중요: 체력이 0이 되었을 때 결과창 띄우기
         if (currentHealth <= 0)
         {
             FindObjectOfType<ResultManager>().ShowResult();
@@ -34,49 +38,53 @@ public class PlayerHeart : MonoBehaviour
     {
         for (int i = 0; i < hearts.Length; i++)
         {
-            if (i < currentHealth)
-            {
-                hearts[i].SetActive(true); 
-            }
-            else
-            {
-                hearts[i].SetActive(false); 
-            }
+            // 현재 체력보다 낮은 인덱스의 하트만 켭니다.
+            hearts[i].SetActive(i < currentHealth);
         }
     }
-    public GameObject burstEffectPrefab;
 
-public void ReduceHeart(int index)
-{
-    if (hearts[index].activeSelf)
+    public void ReduceHeart(int index)
     {
-      
+        // 인덱스 범위 확인 및 하트가 활성화 상태인지 체크
+    if (index >= 0 && index < hearts.Length && hearts[index].activeSelf)
+    {
+       
         GameObject burst = Instantiate(burstEffectPrefab, hearts[index].transform.position, Quaternion.identity);
         
-        // 2. 파티클을 UI 레이어(Canvas)에 보이게 설정 (UI 하트일 경우 중요)
         burst.transform.SetParent(hearts[index].transform.parent);
+
+        burst.transform.localPosition = new Vector3(burst.transform.localPosition.x, burst.transform.localPosition.y, 0);
         burst.transform.localScale = Vector3.one;
 
-        // 3. 하트 비활성화
         hearts[index].SetActive(false);
+
         Destroy(burst, 1f);
     }
-}
-private void OnCollisionEnter2D(Collision2D collision)
-{
-   if (collision.gameObject.CompareTag("Monster"))
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (currentHealth > 0)
+       if (collision.gameObject.CompareTag("Monster"))
+    {
+        Monster monster = collision.gameObject.GetComponent<Monster>();
+        int damageToTake = 1; // 기본값
+
+        if (monster != null)
         {
-            // 지워야 할 하트의 번호 (체력이 3이면 2번 하트, 2면 1번 하트 삭제)
-            int targetIndex = currentHealth - 1;
+            damageToTake = monster.attackDamage; // 몬스터의 공격력을 가져옴
+            monster.ApplyKnockback(transform.position);
+        }
 
-            // 하트 터지는 효과 실행 (이 함수 안에서 하트가 SetActive(false) 됨)
-            ReduceHeart(targetIndex);
-
-            // 실제 체력 수치 감소
-            TakeDamage(1);
+        // 가져온 공격력 수치만큼 반복해서 하트를 터뜨림
+        for (int i = 0; i < damageToTake; i++)
+        {
+            if (currentHealth > 0)
+            {
+                int targetIndex = currentHealth - 1;
+                ReduceHeart(targetIndex); // 하트 이펙트
+                TakeDamage(1);            // 체력 감소
+            }
         }
     }
-}
+    }
 }
